e5a3d647eb8f3ad7b4f0896d0bd20a7196c5f74f
Ronei Lopes <roneilopis@gmail.com>
2016-09-07 19:22:41 -0400

    Salvamento funcionando

--- application/views/usuarios/add.php
+++ application/views/usuarios/add.php
@@ -2,26 +2,38 @@
 
     <h3 class="page-header">Adicionar Usuario</h3>
 
-    <form action="<?= base_url('usuario/add') ?>" method="post">
+    <form action="<?= base_url('usuarios/add') ?>" method="post">
         <div class="row">
             <div class="form-group col-md-4">
                 <label for="exampleInputEmail1">Nome</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+                <input type="text" name="nome" class="form-control" id="exampleInputEmail1" placeholder="Digite o Nome">
             </div>
             <div class="form-group col-md-4">
+                <label for="exampleInputEmail1">Sobrenome</label>
+                <input type="text" name="sobrenome" class="form-control" id="exampleInputEmail1" placeholder="Digite o Sobrenome">
+            </div>
+        </div>
+        <div class="row">
+            <div class="form-group col-md-4">
                 <label for="exampleInputEmail1">Login</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+                <input type="text" name="login"class="form-control" id="exampleInputEmail1" placeholder="Digite o Login">
             </div>
             <div class="form-group col-md-4">
                 <label for="exampleInputEmail1">Senha</label>
-                <input type="text" nome="password"class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+                <input type="password" name="senha" class="form-control" id="exampleInputEmail1" placeholder="Digite a Senha">
             </div>
         </div>
 
-        <div class="row">
-            <div class="form-group col-md-3">
-                <label for="exampleInputEmail1">Campo Quatro</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Digite o valor">
+         <div class="row">
+            <div class="form-group col-md-4">
+                <label for="grupo">Grupo</label>
+                <select name="grupo" id="grupo" class="form-control" placeholder="Selecione">
+                    <option> Selecione</option>
+                    <option value="gerente" > Gerencia</option>
+                    <option value="financa" > Financeiro</option>
+                    <option value="diretor" > Diretoria</option>
+                    <option value="ti" > Informática</option>
+                </select>
             </div>
         </div>
 
@@ -30,7 +42,7 @@
         <div class="row">
             <div class="col-md-12">
                 <intput type="hidden" name="submit"/>
-                <button type="submit" class="btn btn-primary">Salvar</button>
+                <button name="submit" type="submit" class="btn btn-primary">Salvar</button>
                 <a href="<?=base_url('usuarios')?>" class="btn btn-default">Cancelar</a>
             </div>
         </div>
