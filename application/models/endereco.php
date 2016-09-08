57bf08337cc8e76f79aac4add37625280393995e
MarceloSobrinho <sobrinho11191@gmail.com>
2016-09-07 22:21:01 -0400

    add endereço

---
+++ application/models/endereco.php
@@ -0,0 +1,16 @@
+<?php
+
+class Endereco extends Model{
+
+	protected $tabela = 'enderecos';
+	#protected $one_to_one = array();
+	#protected $one_to_many = array();
+	#protected $many_to_many = array();
+
+	public function __construct(){
+		parent::__construct();
+	}
+}
+class clientes {
+    //put your code here
+}
