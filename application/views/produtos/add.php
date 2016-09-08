3ec847a92cecd7c624cab59bc49a68bb752f9b4d
Aline Oliveira <aliine.belle20@gmail.com>
2016-09-07 22:43:38 -0400

    Views final

--- application/views/produtos/add.php
+++ application/views/produtos/add.php
@@ -1,2 +1,57 @@
-<?php
+<div id="main" class="container-fluid">
+  
+  <h3 class="page-header">Adicionar Funcionários'</h3>
+  
+  <form action="<?= base_url('produtos/add')?>" method="post">
+  	<div class="row">
+	  <div class="form-group col-md-4">
+  	  	<label for="descricao">Descrição</label>
+                <input type="text" class="form-control" name="descricao" id="descricao" placeholder="Descrição">
+  	  </div>
+          <div class="form-group col-md-4">
+  	  	<label for="categoria">Categoria</label>
+                <input type="text" class="form-control" name="categoria" id="categoria" placeholder="Categoria">
+  	  </div>
+          <div class="form-group col-md-4">
+  	  	<label for="quantidade">Quantidade</label>
+                <input type="number" class="form-control" name="quantidade" id="quantidade" placeholder="Quantidade">
+  	  </div>
+          <div class="form-group col-md-4">
+  	  	<label for="telefone">Valor</label>
+                <input type="number" class="form-control" name="valor" id="valor" placeholder="Valor">
+  	  </div>
+	</div>
+      
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="categoria">Categoria</label>
+                    <select name="categoria" id="categoria" class="form-control">
+                        <option value=""></option>
+                      
+                    </select>
+                </div>
+            </div>
+      
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="fornecedor">Fornecedor</label>
+                    <select name="fornecedor" id="fornecedor" class="form-control">
+                        <option value=""> </option>
+                        
+                    </select>
+                </div>
+            </div>
+	<hr>
+	
+	<div class="row">
+	  <div class="col-md-12">
+	  	<button name="submit" type="submit" class="btn btn-primary">Atualizar</button>
+		<a href="<?= base_url('produtos')?>" class="btn btn-default">Cancelar</a>
+	  </div>
+	</div>
+
+  </form>
+ </div>
+
+
 
