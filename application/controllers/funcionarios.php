a14da722db4d9d88746c25252e4976eb10f7da67
Aline Oliveira <aliine.belle20@gmail.com>
2016-09-09 09:29:18 -0400

    Funcionários add (select funcionando)

--- application/controllers/funcionarios.php
+++ application/controllers/funcionarios.php
@@ -12,14 +12,26 @@ class Funcionarios extends Controller {
     }
 
     public function add() {
+        
+        $end = new Endereco();
+        $end->get();
+        $this->data['dadosEnd'] = $end->all_to_array();
+        
+        $cargo = new Cargo();
+        $cargo->get();
+        $this->data['dadosCargo'] = $cargo->all_to_array();
+        
+        $this->view('funcionarios/add', $this->data);
+        
         if(isset ($_POST['submit'])){
             $novo = $this->post_to_obj(array('nome','entrada','telefone','cargo_id','endereco_id'), new Funcionario());
             $novo->save();
-            $this->render('funcionarios/index');
+            $this->index();
         }else{
             $this->render('funcionarios/add');
         }
     }
+
     public function edit(){
          $this->render('funcionarios/edit');
     }
