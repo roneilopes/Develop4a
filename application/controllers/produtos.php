a7689402831e74f991de72a98d77751aa9b79134
Ronei Lopes <roneilopis@gmail.com>
2016-09-09 23:35:09 -0400

    Adição de Produtos

--- application/controllers/produtos.php
+++ application/controllers/produtos.php
@@ -15,6 +15,14 @@ class Produtos extends Controller{
     }
     
     public function add(){
+        $for = new Fornecedor();
+        $for->get();
+        $this->data['dadosFor'] = $for->all_to_array();
+        
+        $cat = new Categoria();
+        $cat->get();
+        $this->data['dadosCat'] = $cat->all_to_array();
+        
         if(isset ($_POST['submit'])){
             $novo = $this->post_to_obj(array('descricao','quantidade','fornecedor_id','categoria_id','quantidade','valor'), new Produto());
             $novo->save();
@@ -29,5 +37,4 @@ class Produtos extends Controller{
     }
 
 }
-
 ?>
