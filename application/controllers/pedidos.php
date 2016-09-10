4aac91874f9916358975050c9fc86e6c5efd90d7
Ronei Lopes <roneilopis@gmail.com>
2016-09-09 21:50:56 -0400

    Pedido implementado mas com bug

--- application/controllers/pedidos.php
+++ application/controllers/pedidos.php
@@ -8,12 +8,24 @@ class Pedidos extends Controller {
     }
 
     public function index() {
+        $ped = new Pedido();
+        $ped->get();
+        $this->data['valores'] = $ped->all_to_array();
         $this->render('pedidos/index');
     }
 
     public function add() {
+        $fun = new Funcionario();
+        $fun->get();
+        $this->data['dadosFun'] = $fun->all_to_array();
+        
+        $cli = new Cliente();
+        $cli->get();
+        $this->data['dadosCli'] = $cli->all_to_array();
+        
         if(isset ($_POST['submit'])){
-            $novo = $this->post_to_obj(array('id','funcionario_id','cliente_id','data_cadastro'), new Pedido());
+            $novo = $this->post_to_obj(array('funcionario_id','cliente_id'), new Pedido());
+            $novo->data_cadastro = date('Y-m-d H:i:s');
             $novo->save();
             $this->render('pedidos/index');
         }else{
--- application/views/pedidos/add.php
+++ application/views/pedidos/add.php
@@ -1,44 +1,55 @@
 <div id="main" class="container-fluid">
-  
-  <h3 class="page-header">Adicionar Pedidos'</h3>
-  
-  <form action="<?= base_url('pedidos/add')?>" method="post">
-      
-  	<div class="row">
-  	  <div class="form-group col-md-4">
-  	  	<label for="id">ID</label>
-                <input type="number" class="form-control" id="id" placeholder="Id">
-  	  </div>
-	   <div class="row">
-            <div class="form-group col-md-4">
-                <label for="funcionario">Funcionário</label>
-                <select name="funcionario" id="funcionario" class="form-control">
-                    <option value="gerente"> Gerente</option>
-                    <option value="diretor"> Diretor</option>
-                    <option value="tecnico"> Técnico</option>   
-                </select>
-            </div>
-               
+    <h3 class="page-header">Adicionar Pedidos'</h3>
+    <form action="<?= base_url('pedidos/add') ?>" method="post">
+        <div class="row">
             <div class="row">
-            <div class="form-group col-md-4">
-                <label for="cliente">Cliente</label>
-                <select name="cliente" id="cliente" class="form-control">
-                    <option value="Ronei"> Ronei</option>
-                </select>
+                <div class="form-group col-md-4">
+                    <label for="funcionario">Funcionário</label>
+                    <select name="funcionario_id" id="funcionario" class="form-control" placeholder="Selecione" autofocus="">
+                        <option disabled selected> Selecione Funcionário</option>
+                        <?php
+                        $i = 0;
+                        while ($i < count($dadosFun)) {
+                            try {
+                                echo "<option value='" . $dadosFun[$i]['id'] . "'> Nome: {$dadosFun[$i]['nome']} - Fone: {$dadosFun[$i]['telefone']} </option>";
+                                $i++;
+                            } catch (Exception $ex) {
+                                return $ex->getMessage();
+                            }
+                        }
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="row">
+                <div class="form-group col-md-4">
+                    <label for="cliente">Cliente</label>
+                    <select name="cliente_id" id="funcionario" class="form-control" placeholder="Selecione">
+                        <option disabled selected> Selecione Cliente</option>
+                        <?php
+                        $i = 0;
+                        while ($i < count($dadosCli)) {
+                            try {
+                                echo "<option value='" . $dadosCli[$i]['id'] . "'> Nome: {$dadosCli[$i]['nome']} - Fone: {$dadosCli[$i]['telefone']} </option>";
+                                $i++;
+                            } catch (Exception $ex) {
+                                return $ex->getMessage();
+                            }
+                        }
+                        ?>
+                    </select>
+                </div>
             </div>
         </div>
-       </div>
-	<hr>
-	
-	<div class="row">
-	  <div class="col-md-12">
-	  	<button name="submit" type="submit" class="btn btn-primary">Salvar</button>
-		<a href="<?= base_url('pedidos')?>" class="btn btn-default">Cancelar</a>
-	  </div>
-	</div>
-     
+        <hr>
 
-  </form>
- </div>
+        <div class="row">
+            <div class="col-md-12">
+                <button name="submit" type="submit" class="btn btn-primary">Salvar</button>
+                <a href="<?= base_url('pedidos') ?>" class="btn btn-default">Cancelar</a>
+            </div>
+        </div>
+    </form>
+</div>
 
 
